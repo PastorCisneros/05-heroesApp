bf63b9690daf44ff16ac7a48815aa47f832a446a
Pastor Cisneros <pasada.65@gmail.com>
2022-10-18 19:43:06 -0500

    heroe id

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 
@@ -20,9 +22,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   exports:[
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
     MatListModule,
     MatIconModule,
     MatSidenavModule,
+    MatProgressSpinnerModule,
     MatToolbarModule
   ]
 })
